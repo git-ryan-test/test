4406592f122dff6b21b72e5737141832e454e892
Ryan <ryanspaeth@outlook.de>
2023-04-19 15:29:42 +0200

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,7 @@
+/* const {test, expect} = require('@playwright/test');
+
+test("First Test", async ({page})=> {
+ //write testing code here
+ await page.goto("https://playwright.dev");
+ await page.pause();
+}) */
